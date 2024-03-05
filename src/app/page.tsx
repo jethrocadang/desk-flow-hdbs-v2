61833c5fb751b714f0ee13813e48995e320c349a
John Erish Berboso <>
2024-03-05 14:00:07 +0800

    approved topnav & button components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,24 +8,6 @@ export default function Home() {
   return (
     <div>
       <TopNav />
-      <div className=" mt-28 flex flex-col">
-        <div>
-        <Button size='small' variant="light"  onClick={() => alert('Primary button clicked')}>
-          Sign Up
-        </Button>
-        </div>
-        <div>
-        <Button size='small' variant="primary"  onClick={() => alert('Primary button clicked')}>
-          Sign In
-        </Button>
-        </div>
-        <Button size='small' variant="danger"  onClick={() => alert('Primary button clicked')}>
-          Sign In
-        </Button>
-        <Button size='small' variant="light"  onClick={() => alert('Primary button clicked')}>
-          Sign In
-        </Button>
       </div>
-    </div>
   );
 }
