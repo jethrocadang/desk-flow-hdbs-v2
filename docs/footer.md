45d6d8dcfb8dc66c5acdc209eebd5882cc4ffdb4
John Michael Umali <@johnmichaelumali>
2024-03-05 21:49:24 +0800

    footer added

---
+++ docs/footer.md
@@ -0,0 +1,16 @@
+## Footer docs
+
+You dont have to intall anything
+check the bg-image has been pull to your device.
+
+```bash
+to see the interface run to your terminal
+
+npm run dev
+# or
+yarn dev
+# or
+pnpm dev
+# or
+bun dev
+```
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import Image from "next/image";
 import React from "react";
 import Button from "./ui/toplevelComponents/Button";
 import TopNav from "./ui/toplevelComponents/TopNav";
+import Footer from "./ui/toplevelComponents/Footer";
 
 export default function Home() {
   return (
@@ -26,6 +27,7 @@ export default function Home() {
           Sign In
         </Button>
       </div>
+     <Footer />
     </div>
   );
 }
