8bd2c7375fd29c67f72494a242234ba28f78b7d3
John Michael Umali <@johnmichaelumali>
2024-03-05 08:02:05 +0800

    TopNav added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,23 +2,26 @@
 import Image from "next/image";
 import React from "react";
 import Button from "./ui/toplevelComponents/Button";
+import TopNav from "./ui/toplevelComponents/TopNav";
 
 export default function Home() {
   return (
     <div>
-      <Button size='small' variant="secondary"  onClick={() => alert('Primary button clicked')}>
-        Sign In
-      </Button>
-      <Button size='small' variant="primary"  onClick={() => alert('Primary button clicked')}>
-        Sign In
-      </Button>
-      <Button size='small' variant="danger"  onClick={() => alert('Primary button clicked')}>
-        Sign In
-      </Button>
-      <Button size='small' variant="light"  onClick={() => alert('Primary button clicked')}>
-        Sign In
-      </Button>
-      
+      <TopNav />
+      <div className=" mt-28">
+        <Button size='small' variant="secondary"  onClick={() => alert('Primary button clicked')}>
+          Sign In
+        </Button>
+        <Button size='small' variant="primary"  onClick={() => alert('Primary button clicked')}>
+          Sign In
+        </Button>
+        <Button size='small' variant="danger"  onClick={() => alert('Primary button clicked')}>
+          Sign In
+        </Button>
+        <Button size='small' variant="light"  onClick={() => alert('Primary button clicked')}>
+          Sign In
+        </Button>
+      </div>
     </div>
   );
 }
---
+++ src/app/ui/toplevelComponents/TopNav.tsx
@@ -0,0 +1,74 @@
+'use client';
+import React from 'react'
+import Link from 'next/link';
+import { useState } from 'react'; 
+import Button from './Button'
+import { RxHamburgerMenu, RxCross2 } from "react-icons/rx";
+
+
+
+export default function TopNav() {
+  // for burger Navigation
+  const [navbar, setNavbar] = useState(false);
+  return (
+    <div>
+      {/* nav container */}
+      <nav className="w-full border-b-2 border-slate-400 bg-white fixed top-0 left-0 right-0 z-10">
+        <div className="justify-between px-4 mx-auto lg:max-w-7xl md:items-center md:flex md:px-8">
+          <div>
+            <div className="flex items-center justify-between py-3 md:py-5 md:block">
+              {/* LOGO or NAME  */}
+              <Link href="#">
+                <h2 className="text-2xl text-blue-700 font-bold ">Desk<span className='text-blue-500'>Flow</span> </h2>
+              </Link>
+              {/* BURGERNAV FOR MOBILE */}
+              <div className="md:hidden">
+                <button
+                  className="p-2 text-gray-700 rounded-md outline-none focus:border-gray-400 focus:border duration-300"
+                  onClick={() => setNavbar(!navbar)}
+                >
+                  {navbar ? (
+                    // close nav
+                    <div>
+                      <RxCross2 width={40} height={40} />
+                    </div>
+                  ) : (
+                   // open nav
+                    <div>
+                      <RxHamburgerMenu width={40} height={40} />
+                    </div>
+                  )}
+                </button>
+              </div>
+            </div>
+          </div>
+          <div>
+
+            {/* Sign in & Sign up button */}
+            
+            <div
+              className={`flex-1 justify-self-center pb-3 mt-8 md:block md:pb-0 md:mt-0 ${
+                // checking if navburger is true or false
+                navbar ? 'p-12 md:p-0 block duration-500 bg-transparent' : 'hidden'
+              }`}
+            >
+              <ul className="h-screen md:pr-5 md:h-auto items-center justify-center md:flex duration-300">
+                <li className="pb-6 text-sm font-semibold pt-4 md:px-2 text-center md:hover:bg-transparent">
+                <Button size='large' variant="light"  onClick={() => alert('Primary button clicked')}>
+                  Sign Up
+                </Button>
+                </li>
+                <li className="pb-6 text-sm font-semibold py-4 md:px-2 text-center md:hover:bg-transparent">
+                <Button size='large' variant="primary"  onClick={() => alert('Primary button clicked')}>
+                  Sign In
+                </Button>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </nav>
+    </div>
+
+  )
+}
