5ee143b8513150606e7be72d0c0440e4ce57fd3a
jethrocadang <116892945+jethrocadang@users.noreply.github.com>
2024-03-15 14:07:08 +0800

    feature/Auth: signIn[login func]

--- auth.ts
+++ auth.ts
@@ -3,15 +3,56 @@ import { PrismaAdapter } from "@auth/prisma-adapter";
 
 import authConfig from "auth.config";
 import { db } from "@/lib/prisma";
+import { getUserById } from "@/data/user";
+import { UserRole } from "@prisma/client";
+
+// Declare role to typescript
+declare module "next-auth" {
+  interface User {
+     role: UserRole ;
+  }
+}
 
 // Auth configuration
 export const {
   handlers: { GET, POST },
   auth,
   signIn,
-  signOut
+  signOut,
 } = NextAuth({
   adapter: PrismaAdapter(db),
   session: { strategy: "jwt" },
+  callbacks: {
+    // Check if Email Verfied, if not "You shall not pass!!!"
+    async signIn({user}){
+
+      const existingUser = getUserById(user.id)
+      if(!existingUser && !(await existingUser).emailVerified){
+        return false
+      }
+    },
+    // Get session {role}
+    async session({ token, session }) {
+      if (token.sub && session.user) {
+        session.user.id = token.sub;
+      }
+      if (token.role && session.user) {
+        session.user.role = token.role as UserRole;
+      }
+      return session;
+    },
+    // Use token to get user Id
+    async jwt({ token }) {
+      if (!token.sub) return token;
+
+      const existingUser = await getUserById(token.sub);
+
+      if (!existingUser) return token;
+
+      token.role = existingUser.role;
+
+      return token;
+    },
+  },
   ...authConfig,
 });
--- src/app/(_protected)/user/dashboard/(overview)/page.tsx
+++ src/app/(_protected)/user/dashboard/(overview)/page.tsx
@@ -2,6 +2,7 @@ import { auth, signOut } from "auth";
 
 export default async function Page() {
   const session = await auth();
+
   return (
     <div>
       user {JSON.stringify(session)}{" "}
